b89f2e2a4e79dac78748116f85cada1ddf380214
AlsuV <165937069+AlsuV@users.noreply.github.com>
2024-07-20 23:32:06 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
-
-primes = list[numbers]   # простые числа ...Primes: [2, 3, 5, 7, 11, 13]
-not_primes = list[numbers]  # не простые числа ...Not Primes: [4, 6, 8, 9, 10, 12, 14, 15]
 i = 0
+primes = numbers[i]   # простые числа ...Primes: [2, 3, 5, 7, 11, 13]
+not_primes = numbers[i]  # не простые числа ...Not Primes: [4, 6, 8, 9, 10, 12, 14, 15]
 n = 0
-
 for i in range(len(numbers)):
     for n in numbers:
         if n < 2:
@@ -13,4 +11,4 @@ for i in range(len(numbers)):
             continue
         else:
             print(not_primes)
-        break
+            break
